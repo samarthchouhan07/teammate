9ae5b716b4d0dd5420cd2680fd9ac58e9632a044
samarthchouhan07 <samarthchouhan29544@gmail.com>
2024-11-01 22:42:07 +0530

    create server modal done

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import { Open_Sans } from "next/font/google";
 import { ClerkProvider } from "@clerk/nextjs";
 import { ThemeProvider } from "@/components/providers/theme-provider";
 import { cn } from "@/lib/utils";
+import { ModalProvider } from "@/components/providers/modal-provider";
 
 const font = Open_Sans({
   subsets: ["latin"],
@@ -23,15 +24,14 @@ export default function RootLayout({
   return (
     <ClerkProvider afterSignOutUrl={"/sign-in"}>
       <html lang="en" suppressHydrationWarning>
-        <body className={cn(font.className,
-          "bg-white dark:bg-[#313338]"
-        )}>
+        <body className={cn(font.className, "bg-white dark:bg-[#313338]")}>
           <ThemeProvider
             attribute="class"
             defaultTheme="dark"
             enableSystem={false}
             storageKey="teammate-theme"
           >
+            <ModalProvider />
             {children}
           </ThemeProvider>
         </body>
